efe749f9aa57e4cf798c8501782191855cc3cab5
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-03-20 07:30:33 +0800

    event clicked example

--- pertemuan3/script.js
+++ pertemuan3/script.js
@@ -1,21 +1,27 @@
 // NOTE: DOM SELECTOR
 
-const paragraphID = document.getElementById("test-1"); //return 1 element;
-const paragraphClass = document.getElementsByClassName("test-2")[0]; //return htmlcollections / array;
-const paragraphByTagName = document.getElementsByTagName("p"); // return htmlcollections / array;
-const paragraphByQuerySelector = document.querySelector("p"); //return 1 element;
-const paragraphByQuerySelectorAll = document.querySelectorAll("p"); //return NodeList / array;
+// const paragraphID = document.getElementById("test-1"); //return 1 element;
+// const paragraphClass = document.getElementsByClassName("test-2")[0]; //return htmlcollections / array;
+// const paragraphByTagName = document.getElementsByTagName("p"); // return htmlcollections / array;
+// const paragraphByQuerySelector = document.querySelector("p"); //return 1 element;
+// const paragraphByQuerySelectorAll = document.querySelectorAll("p"); //return NodeList / array;
 
-paragraphID.innerText = "Saya rubah teksnya menggunakan Javascipt";
-console.log(paragraphByQuerySelectorAll);
+// paragraphID.innerText = "Saya rubah teksnya menggunakan Javascipt";
+// console.log(paragraphByQuerySelectorAll);
 
-paragraphByQuerySelectorAll.forEach(function (paragraph) {
-    paragraph.textContent = "Saya rubah teksnya menggunakan Javascipt";
+// paragraphByQuerySelectorAll.forEach(function (paragraph) {
+//     paragraph.textContent = "Saya rubah teksnya menggunakan Javascipt";
+// });
+
+// paragraphByQuerySelector.innerText = "Saya rubah teksnya menggunakan Javascipt";
+
+// for (let i = 0; i < paragraphByTagName.length; i++) {
+//     paragraphByTagName[i].textContent =
+//         "Saya rubah teksnya menggunakan Javascipt";
+// }
+
+const button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+    button.innerText = "sudah di click";
 });
-
-paragraphByQuerySelector.innerText = "Saya rubah teksnya menggunakan Javascipt";
-
-for (let i = 0; i < paragraphByTagName.length; i++) {
-    paragraphByTagName[i].textContent =
-        "Saya rubah teksnya menggunakan Javascipt";
-}
