8d383a643f8b708ee0182f2e2cacc25f2a964393
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-03-20 07:19:05 +0800

    keywod let & const, masalah var dan solusinya

--- pertemuan3/fundamental.js
+++ pertemuan3/fundamental.js
@@ -3,3 +3,16 @@
 var name = "danbo";
 
 console.log(name);
+
+console.log(name);
+
+let name = "Danbo";
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
+
+const name = "dandi";
+name = "danbo";
