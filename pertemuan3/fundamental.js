7ac8ee271db2e95cd96e3d93089d1f6aff72b2dd
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-03-20 07:19:38 +0800

    data type in js

--- pertemuan3/fundamental.js
+++ pertemuan3/fundamental.js
@@ -1,18 +1,28 @@
 // var
 
-var name = "danbo";
+// var name = "danbo";
 
-console.log(name);
+// console.log(name);
 
-console.log(name);
+// console.log(name);
 
-let name = "Danbo";
+// let name = "Danbo";
 
-for (var i = 0; i < 5; i++) {
-    console.log(i);
-}
+// for (var i = 0; i < 5; i++) {
+//     console.log(i);
+// }
 
-console.log(i);
+// console.log(i);
 
-const name = "dandi";
-name = "danbo";
+// const name = "dandi";
+// name = "danbo";
+
+// console.log(typeof "ini adalah string");
+// console.log(typeof 12);
+// console.log(typeof true, typeof false);
+// console.log(typeof [1, "mhs", true]);
+// console.log(typeof { key: 2 });
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof NaN);
+// console.log(Symbol(23) === Symbol(23));
